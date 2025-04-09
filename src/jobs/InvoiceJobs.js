31bb831832f50ea84958bb7c53e7b6b4feada99e
] <wellingtons.bezerra@hotmail.com>
2025-04-08 23:28:10 -0300

    chore: include call function inssueRandomInvoices to cron job execute

--- src/jobs/InvoiceJobs.js
+++ src/jobs/InvoiceJobs.js
@@ -1,8 +1,11 @@
 import cron from "node-cron";
 import logger from "winston";
 
+import { inssueRandomInvoices } from "../services/InvoiceService.js";
+
 function startJobs() {
-    const sendInvoicesJob = cron.schedule("* * * * *", () => {
+    const sendInvoicesJob = cron.schedule("* * * * *", async () => {
+        await inssueRandomInvoices();
         logger.info(`[${new Date().toISOString()}] Sending invoices to startbank`);
     });
 
